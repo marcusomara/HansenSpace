611f4d8bca12be2e79727ef107a1141f003f8f0e
marcusomara <marcusomara@live.co.uk>
2017-12-22 11:39:22 +0000

    First commit, says on the tin!

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+﻿
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float panSpeed = 20f;
+	public float rotSpeed = 1f;
+
+	//Update is called once per frame
+	void Update(){
+
+		Vector3 pos = transform.position;
+		Quaternion rot = transform.rotation;
+
+		//x-y-z movement
+		if (Input.GetKey("w"))
+		{
+			pos.z += panSpeed * Time.deltaTime;	
+		}
+		if (Input.GetKey("s"))
+		{
+			pos.z -= panSpeed * Time.deltaTime;	
+		}
+		if (Input.GetKey("a"))
+		{
+			pos.x -= panSpeed * Time.deltaTime;	
+		}
+		if (Input.GetKey("d"))
+		{
+			pos.x += panSpeed * Time.deltaTime;	
+		}		
+		if (Input.GetKey("q"))
+		{
+			pos.y -= panSpeed * Time.deltaTime;	
+		}
+		if (Input.GetKey("e"))
+		{
+			pos.y += panSpeed * Time.deltaTime;	
+		}
+
+		//rotational movement
+		if (Input.GetKey("i"))
+		{
+			rot.x -= rotSpeed * Time.deltaTime;	
+		}
+		if (Input.GetKey("k"))
+		{
+			rot.x += rotSpeed * Time.deltaTime;	
+		}
+		if (Input.GetKey("j"))
+		{
+			rot.y -= rotSpeed * Time.deltaTime;	
+		}
+		if (Input.GetKey("l"))
+		{
+			rot.y += rotSpeed * Time.deltaTime;	
+		}
+
+
+		transform.position = pos;
+		transform.rotation = rot;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+    {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		float moveUpDown = Input.GetAxis ("up/down");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, moveUpDown, moveVertical);
+
+		rb.AddForce(movement *speed );
+
+    }
+}
+
